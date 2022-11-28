5a8430fd76ce9e16ef2365ca1ea03c4290e2a67a
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-11-28 08:06:51 +0800

    Ajout Inocybe maculata

--- deploy/create_ner.sh
+++ deploy/create_ner.sh
@@ -1,1 +1,11 @@
-torch-model-archiver --m
+torch-model-archiver --model-name BERTForNER_en  \
+	--version 1.0 --serialized-file ./ner_en/pytorch_model.bin \
+	--handler ./deploy/handler.py \
+	--extra-files "./ner_en/config.json,./ner_en/setup_config.json,./ner_en/vocab.txt,./deploy/model.py" -f
+
+
+sudo cp ./BERTForNER_en.mar /home/model-server/model-store/
+
+# ./start_server.sh
+# delete the exist model
+curl -X DELETE http://localhost:3001/mode
