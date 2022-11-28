d92ed44e5fc0e1db213b5c6003172bae964e608a
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-11-28 14:08:40 +0800

    Upgraded aws-cli

---
+++ deploy/handler_ner.py
@@ -0,0 +1,36 @@
+
+from abc import ABC
+import enum
+import json
+import logging
+import os
+import ast
+from posixpath import realpath
+import torch
+import transformers
+import torch.nn as nn
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    AutoModelForQuestionAnswering,
+    AutoModelForTokenClassification,
+    AutoConfig
+)
+from transformers.models.bert.configuration_bert import BertConfig
+from ts.torch_handler.base_handler import BaseHandler
+
+logger = logging.getLogger(__name__)
+logger.info("Transformers version %s",transformers.__version__)
+
+
+from model import BertForNER, BertForRelationClassification
+
+
+class BertForNERHandler(BaseHandler, ABC):
+    """
+    Transformers handler class for sequence, token classification and question answering.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.initialized = False
