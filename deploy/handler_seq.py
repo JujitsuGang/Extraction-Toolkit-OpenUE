89994ee634484c3cbd034cf3850151db25885fce
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2022-11-30 07:31:40 +0800

    merge with devel

--- deploy/handler_seq.py
+++ deploy/handler_seq.py
@@ -9,4 +9,16 @@ import torch
 import transformers
 import torch.nn as nn
 from transformers import (
-    AutoModelForSequenceClas
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    AutoModelForQuestionAnswering,
+    AutoModelForTokenClassification,
+)
+from transformers.models.bert.configuration_bert import BertConfig
+from ts.torch_handler.base_handler import BaseHandler
+
+logger = logging.getLogger(__name__)
+logger.info("Transformers version %s",transformers.__version__)
+
+
+from
