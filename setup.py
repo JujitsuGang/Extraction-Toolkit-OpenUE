558dc6c0cc63ce112c8ab42955db8f3a701c5f15
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-08-09 18:47:06 +0800

    update snapshots

--- setup.py
+++ setup.py
@@ -23,4 +23,9 @@ setuptools.setup(
     packages=setuptools.find_packages("src"),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6'
+)
