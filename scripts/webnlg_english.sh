8e19955c663252f3f5870074b9a6ccec9069b5d2
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-08-09 10:21:34 +0800

    Logging fixes

--- scripts/webnlg_english.sh
+++ scripts/webnlg_english.sh
@@ -4,4 +4,18 @@ dataset="WebNLG"
 
 python main.py --gpus "0," --max_epochs 10  \
     --data_class REDataset \
-    --litmodel_class RELitMode
+    --litmodel_class RELitModel \
+    --model_class BertForNER \
+    --task_name ner \
+    --batch_size 16 \
+    --model_name_or_path bert-large-uncased \
+    --max_seq_length 256 \
+    --check_val_every_n_epoch 1 \
+    --data_dir ./dataset/${dataset}  \
+    --overwrite_cache \
+    --lr 1e-5 
+
+
+python main.py --gpus "0," --max_epochs 20  \
+    --data_class REDataset \
+    --litmo
