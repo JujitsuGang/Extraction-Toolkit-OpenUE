7d193458b19ceabcb28cf54c2d515ede53f31ebc
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-08-09 12:37:48 +0800

    Improved CSS

--- setup.py
+++ setup.py
@@ -2,4 +2,16 @@ import os
 import setuptools
 
 with open("README.md", "r") as fh:
-    l
+    long_description = fh.read()
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="openue",
+    version="0.2.5",
+    author="zxlzr",
+    a
