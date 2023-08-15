4ab13f3f5be882ba55996c2b684e6ce8b965de2b
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-08-15 13:14:11 +0800

    solaris

--- src/openue/__init__.py
+++ src/openue/__init__.py
@@ -1,2 +1,18 @@
 from .data import REDataset
-from .lit_mod
+from .lit_models import RELitModel, SEQLitModel
+from .models import BertForNER, BertForRelationClassification
+
+import importlib
+import argparse
+
+
+def _import_class(module_and_class_name: str) -> type:
+
+    module_name, class_name = module_and_class_name.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    class_ = getattr(module, class_name)
+	
+    return class_
+
+
+def _
