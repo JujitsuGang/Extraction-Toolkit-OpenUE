df023370233d0c16a5d04234109ecf5674bec28c
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-10-03 16:41:48 +0800

    svm

--- src/openue/data/base_data_module.py
+++ src/openue/data/base_data_module.py
@@ -1,4 +1,26 @@
 """Base DataModule class."""
 from pathlib import Path
 from typing import Dict
-imp
+import argparse
+import os
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+
+class Config(dict):
+    def __getattr__(self, name):
+        return self.get(name)
+
+    def __setattr__(self, name, val):
+        self[name] = val
+
+
+BATCH_SIZE = 8
+NUM_WORKERS = 8
+
+
+class BaseDataModule(pl.LightningDataModule):
+    """
+    Base DataModule.
+    Learn more at ht
