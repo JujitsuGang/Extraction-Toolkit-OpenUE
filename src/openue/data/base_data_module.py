dc9bb332bcc935e5cee347b87ad13193353d1b7a
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-10-03 20:07:46 +0800

    fuse: hotfix truncate_pagecache() issue

--- src/openue/data/base_data_module.py
+++ src/openue/data/base_data_module.py
@@ -30,4 +30,15 @@ class BaseDataModule(pl.LightningDataModule):
         super().__init__()
         self.args = Config(vars(args)) if args is not None else {}
         self.batch_size = self.args.get("batch_size", BATCH_SIZE)
-        self.num_workers = self.args.get("n
+        self.num_workers = self.args.get("num_workers", NUM_WORKERS)
+
+
+    @staticmethod
+    def add_to_argparse(parser):
+        parser.add_argument(
+            "--batch_size", type=int, default=BATCH_SIZE, help="Number of examples to operate on per forward step."
+        )
+        parser.add_argument(
+            "--num_workers", type=int, default=NUM_WORKERS, help="Number of additional processes to load data."
+        )
+    
