84173288d242480a233965f1767e5a64ae12ffed
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2023-10-27 22:09:51 +0800

    Fix background

--- src/openue/data/data_module.py
+++ src/openue/data/data_module.py
@@ -1,2 +1,13 @@
 import os
-from .base_data_m
+from .base_data_module import BaseDataModule
+from .processor import get_dataset
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+
+import logging
+
+logger = logging.getLogger(__name__)
+from .utils import get_labels_ner, get_labels_seq, openue_data_collator_seq, openue_data_collator_ner, openue_data_collator_interactive
+
+
+collator_set = {"ner": openu
