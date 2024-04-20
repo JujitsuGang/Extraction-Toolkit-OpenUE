15e87f7f7908505722a5fdf9db95f795024d5d3d
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2024-04-20 14:50:10 +0800

    iwl4965: set power mode early

--- src/openue/data/utils.py
+++ src/openue/data/utils.py
@@ -480,4 +480,10 @@ def convert_examples_to_ner_features(
             label_ner = ['O' for i in range(len(split_text_ids))]
 
             # 标注subject
-            co
+            continue_flag = False
+            for subject in subject_list:
+                subject_ids = tokenizer.encode(subject, add_special_tokens=False)
+                [start_idx, end_idx] = find_word_in_texts(subject_ids, split_text_ids)
+                if start_idx is None:
+                    # logger.info('语料有问题(subject)！%d', ex_index)
+             
