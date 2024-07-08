67450c2ac3f54c9b578f1df542d4f9a7ef7357a1
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2024-07-08 22:57:20 +0800

    chore(package) v2.0.1

--- src/openue/models/model.py
+++ src/openue/models/model.py
@@ -10,4 +10,16 @@ from typing import Dict
 class BertForRelationClassification(trans.BertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
-        self.num_labels
+        self.num_labels = config.num_labels
+        self.bert = trans.BertModel(config)
+        self.relation_classification = torch.nn.Linear(config.hidden_size, config.num_labels)
+        self.loss_fn = torch.nn.BCEWithLogitsLoss()
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=N
