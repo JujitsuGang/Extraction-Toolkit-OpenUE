a43e44260fea2edd89e7810bb828a2c57b140e7d
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2024-07-08 17:17:37 +0800

    #5933

--- src/openue/models/model.py
+++ src/openue/models/model.py
@@ -2,4 +2,12 @@ import transformers as trans
 import torch
 import pytorch_lightning as pl
 from torch.nn import CrossEntropyLoss, MSELoss
-from transformers.mod
+from transformers.models.auto.configuration_auto import AutoConfig
+from transformers import AutoTokenizer
+from openue.data.utils import get_labels_ner, get_labels_seq, OutputExample
+from typing import Dict
+
+class BertForRelationClassification(trans.BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels
