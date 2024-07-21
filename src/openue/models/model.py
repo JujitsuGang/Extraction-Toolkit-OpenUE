af9be5e5483d33b8276b7b180a645e6e4bfe4d6e
JujitsuGang <105345817+JujitsuGang@users.noreply.github.com>
2024-07-21 08:37:13 +0800

    run all tests

--- src/openue/models/model.py
+++ src/openue/models/model.py
@@ -92,4 +92,15 @@ class BertForNER(trans.BertPreTrainedModel):
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         outputs = self.bert(
-            inp
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        # batc
